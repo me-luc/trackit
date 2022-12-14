fcc8436535adeff60af902315aecdefee991c6a9
Lucas Almeida <lukasalmeida372@gmail.com>
2022-12-14 17:11:34 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HabitsPage from "./pages/HabitsPage";
+import HistoryPage from "./pages/HistoryPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import TodayPage from "./pages/TodayPage";
+
 export default function App() {
 	return (
-		<div className="App">
-			<h1>APP</h1>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/cadastro" element={<SignUpPage />} />
+				<Route path="/habitos" element={<HabitsPage />} />
+				<Route path="/hoje" element={<TodayPage />} />
+				<Route path="/historico" element={<HistoryPage />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
