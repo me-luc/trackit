119d96fa0e932bd2e9fd80031815ad9b41f27bea
Lucas Almeida <lukasalmeida372@gmail.com>
2022-12-14 19:46:50 -0300

    add header layout (mock)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import GlobalStyle from "./globalStyle";
 import HabitsPage from "./pages/HabitsPage";
 import HistoryPage from "./pages/HistoryPage";
 import LoginPage from "./pages/acessPages/LoginPage";
 import SignUpPage from "./pages/acessPages/SignUpPage";
 import TodayPage from "./pages/TodayPage";
+import Header from "./components/header/Header";
 
 export default function App() {
 	return (
-		//<GlobalStyle />
-		<BrowserRouter>
+		<>
 			<GlobalStyle />
-			<Routes>
-				<Route path="/" element={<LoginPage />} />
-				<Route path="/cadastro" element={<SignUpPage />} />
-				<Route path="/habitos" element={<HabitsPage />} />
-				<Route path="/hoje" element={<TodayPage />} />
-				<Route path="/historico" element={<HistoryPage />} />
-			</Routes>
-		</BrowserRouter>
+			<Header />
+		</>
+		// <BrowserRouter>
+		// 	<GlobalStyle />
+		// 	<Routes>
+		// 		<Route path="/" element={<LoginPage />} />
+		// 		<Route path="/cadastro" element={<SignUpPage />} />
+		// 		<Route path="/habitos" element={<HabitsPage />} />
+		// 		<Route path="/hoje" element={<TodayPage />} />
+		// 		<Route path="/historico" element={<HistoryPage />} />
+		// 	</Routes>
+		// </BrowserRouter>
 	);
 }
